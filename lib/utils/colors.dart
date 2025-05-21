d7306dd820cf3f76816c3a511a84312348f1307e
emir yorulmaz <emiryorulmaz@ikmail.com>
2025-05-21 15:20:47 +0300

    Change color scheme

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color background = Color(0xFF000000);
-  static const Color primaryText = Color(0xFFFFFFFF);
-  static const Color secondaryText = Color(0xFFE0E0E0);
-  static const Color border = Color(0xFFAAAAAA);
-  static const Color buttonBackground = Color(0xFF222222);
-  static const Color buttonText = Color(0xFFFFFFFF);
-  static const Color error = Color(0xFF888888);
-  static const Color success = Color(0xFFDDDDDD);
-  static const Color currencypositive = Color(0xFF81C784);
-  static const Color currencynegative = Color(0xFFE57373);
+  static const Color background = Color(0xFF0D0D0D);
+  static const Color primaryText = Color(0xFFFFEBC1);
+  static const Color secondaryText = Color(0xFFFFDDA1);
+  static const Color border = Color(0xFF3A3A3A);
+
+  static const Color buttonBackground = Color(0xFF1A1A1A);
+  static const Color buttonText = primaryText;
+
+  static const Color error = Color(0xFFFF5C5C);
+  static const Color success = Color(0xFF8AFF8A);
+
+  static const Color currencypositive = Color(0xFFB0FFB0);
+  static const Color currencynegative = Color(0xFFFF8A8A);
 
   static const Color confettiColor = primaryText;
   static const Color loginBackground = background;
@@ -18,7 +21,7 @@ class AppColors {
   static const Color loginInputBorder = border;
   static const Color loginErrorText = error;
   static const Color loginButtonBackground = buttonBackground;
-  static const Color loginButtonText = buttonText;
+  static const Color loginButtonText = primaryText;
 
   static const Color sendAmountText = primaryText;
   static const Color sendSuccess = success;
@@ -29,10 +32,10 @@ class AppColors {
   static const Color dialogBorder = border;
   static const Color dialogErrorText = error;
   static const Color dialogButtonBackground = buttonBackground;
-  static const Color dialogButtonText = buttonText;
+  static const Color dialogButtonText = primaryText;
 
   static const Color qrBackground = Colors.white;
   static const Color qrIconColor = primaryText;
 
-  static const Color payingloading = Color(0xFFFFF9C4);
+  static const Color payingloading = Color(0xFFFFD95C);
 }
